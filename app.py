02ca71202f6879276fa5306a9408c964589b2232
Sonali Liyana <sliyanab23@univ.yo.oulu.fi>
2024-06-20 11:20:19 +0300

    refactor code structure

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-from flask import Flask, render_template, request, jsonify
-from rulebot import RuleBot
+from flask import Flask, render_template
+from flask_restful import Api
+from data_model.weather_responses import WeatherResponses
+from routes import StoreResponse, GetResponse
 
 app = Flask(__name__)
+api = Api(app)
+
+# Initialize the WeatherResponses class
+weather_responses = WeatherResponses()
+
+# Add routes to the API
+api.add_resource(StoreResponse, '/store_responses')
+api.add_resource(GetResponse, '/get_response')
 
 @app.route("/")
 def index():
     return render_template('chat.html')
-
-@app.route("/get", methods=["POST"])
-def chat():
-    msg = request.form.get("msg")
-    if msg:
-        bot = RuleBot()
-        response = bot.match_reply(msg)
-        return jsonify({"response": response})
-    return jsonify({"response": "No message received"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
