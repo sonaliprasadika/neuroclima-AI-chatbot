4bc2e72e875b0bd6cc47034038da85d6a59f97c1
sonaliprasadika <sonaliprasadika077@gmail.com>
2024-06-24 07:37:13 +0000

    merge branch version1.0 to main

--- app.py
+++ app.py
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-from flask import Flask, render_template
-from flask_restful import Api
-from resources.storeResource import StoreResource
-from resources.getResource import GetResource
-
-app = Flask(__name__)
-api = Api(app)
-
-# Add routes to the API
-api.add_resource(StoreResource, '/store')
-api.add_resource(GetResource, '/get')
-
-@app.route("/")
-def index():
-    return render_template('chat.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-=======
 from flask import Flask, render_template
 from flask_restful import Api
 from resources.storeResource import StoreResource
@@ -36,4 +16,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(port=3000)
->>>>>>> 6d625fb (added req file and change view)
--- data_model/db.py
+++ data_model/db.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['weather_db']
-=======
-from pymongo import MongoClient
-
-client = MongoClient('mongodb://128.214.253.165:27018/')
-db = client['weather_db']
->>>>>>> 6d625fb (added req file and change view)
