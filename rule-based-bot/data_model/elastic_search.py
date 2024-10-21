8839ad19c8b060101081a53c4d56280ec13c44a6
sonaliprasadika <sonaliprasadika077@gmail.com>
2024-10-21 13:09:59 +0000

    remove rule-based bot

--- rule-based-bot/data_model/elastic_search.py
+++
@@ -1,9 +0,0 @@
-from elasticsearch import Elasticsearch
-
-es = Elasticsearch("http://195.148.31.180:9200")
-# Test the connection
-try:
-    info = es.info().body
-    print(info)
-except Exception as e:
-    print(f"Error connecting to Elasticsearch: {e}")
